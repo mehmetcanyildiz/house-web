444de7c7e2af12d812fdf7bb5d00236c5a74af36
Mehmet Yıldız <thecanyildiz@gmail.com>
2024-05-17 17:37:19 +0300

    some bugs are fixed and color changed.

--- src/app/pages/authentication/login/login.component.ts
+++ src/app/pages/authentication/login/login.component.ts
@@ -13,7 +13,9 @@ export class AppSideLoginComponent implements OnInit {
   loginDTO!: LoginDTO;
   loginForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,private authenticationService: AuthenticationService,private router: Router) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authenticationService: AuthenticationService) {
   }
 
 
